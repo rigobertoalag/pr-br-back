f583005db0793c2e3d56ccb75157a2679d450f5f
rigobertoalag <a20150338@utem.edu.mx>
2021-05-21 10:28:21 -0500

    Roll back and filesytem change

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,10 +35,17 @@ return [
             'root' => storage_path('app'),
         ],
 
-        'public' => [
+        /*'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'root'   => public_path() . '/public',
+            'url' => env('APP_URL').'/public',
+            'visibility' => 'public',
+        ],*/
+
+        'public' => [
+            'driver' => 'local',
+            'root' => public_path(),
             'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
