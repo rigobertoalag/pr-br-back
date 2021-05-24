cb98ba27584799cb45b12cd69a080133d1fe7fb4
rigobertoalag <a20150338@utem.edu.mx>
2021-05-24 18:59:43 -0500

    Init design index table

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,54 +1,114 @@
 @extends('layouts.app')
 
 @php
-$header = "Categorias";
-//$slot = "categories";
+$header = 'Categorias';
 @endphp
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            CMS
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-success" href="{{ route('categories.create' )}}">Añadir categoria</a>
+    <!-- component -->
+    <div class="overflow-x-auto">
+        <div class="min-w-screen min-h-screen flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+            <div class="w-full lg:w-5/6">
+                <div class="pull-right">
+                    <a class="btn btn-success" href="{{ route('categories.create') }}">Añadir categoria</a>
+                </div>
+                <div class="bg-white shadow-md rounded my-6">
+                    <table class="min-w-max w-full table-auto">
+                        <thead>
+                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                <th class="py-3 px-6 text-left">Nombre</th>
+                                <th class="py-3 px-6 text-left">Descripcion</th>
+                                <th class="py-3 px-6 text-center">Fecha de creacion</th>
+                                <th class="py-3 px-6 text-center">Imagen portada</th>
+                                <th class="py-3 px-6 text-center">Estatus</th>
+                                <th class="py-3 px-6 text-center">Creador</th>
+                                <th class="py-3 px-6 text-center">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-gray-600 text-sm font-light">
+                            @foreach ($data as $key => $category)
+                                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                    <td class="py-3 px-6 text-left whitespace-nowrap">
+                                        <div class="flex items-center">
+                                            <span class="font-medium">{{ $category->name }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="py-3 px-6 text-left">
+                                        <div class="flex items-center">
+                                            <span>{{ $category->description }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <div class="flex items-center justify-center">
+                                            <!--<img class="w-6 h-6 rounded-full border-gray-200 border -m-1 transform hover:scale-125" src="https://randomuser.me/api/portraits/men/3.jpg"/>-->
+                                            <span>{{ $category->created_at }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="py-3 px-6 text-center item-center justify-center">
+                                        <!--<span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Active</span>-->
+                                        <img class="w-auto h-14 rounded-sm border-gray-200 border transform hover:scale-125"
+                                            src="/storage/image/{{ $category->image }}">
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <!--<span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Active</span>-->
+                                        <span
+                                            class="bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs">Activo</span>
+                                        |
+                                        <span class="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">Inactivo</span>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <!--<span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Active</span>-->
+                                        <span>User id: {{ $category->user_id }}</span>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <div class="flex item-center justify-center">
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a class="btn btn-info"
+                                                    href="{{ route('categories.show', $category->id) }}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                        stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                    </svg>
+                                                </a>
+                                            </div>
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a class="btn btn-primary"
+                                                    href="{{ route('categories.edit', $category->id) }}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                        stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                    </svg>
+                                                </a>
+                                            </div>
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <form action="{{ route('categories.destroy', $category->id) }}" method="POST">
+                                                <a style="color:black"
+                                                    href="">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        stroke-width="2"
+                                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                </svg>
+                                                </a>
+                                                    @csrf
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
-<!--@if($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
-@endif-->
-
-<table class="table table-bordered">
-    <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Descripcion</th>
-        <th>IMG</th>
-    </tr>
-
-    @foreach($data as $key => $value)
-    <tr>
-        <td>{{ $value->id }}</td>
-        <td>{{ $value->name }}</td>
-        <td>{{ \Str::limit($value->description, 100) }}</td>
-        <td><img src="/storage/image/{{$value->image}}" alt="" width="100"></td>
-        <td>
-            <form action="{{ route('categories.destroy', $value->id) }}" method="POST">
-                <a class="btn btn-info" href="{{ route('categories.show', $value->id)  }}">Detalles</a>
-                <a class="btn btn-primary" href="{{ route('categories.edit', $value->id)  }}">Editar</a>
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Eliminar</button>
-            </form>
-        </td>
-    </tr>
-    @endforeach
-</table>
-
-{!! $data->links() !!}
+    {!! $data->links() !!}
 @endsection
