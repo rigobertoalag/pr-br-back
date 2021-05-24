d8d1d68fa326d12cb45c5a7adeb7129620e2f6f9
rigobertoalag <a20150338@utem.edu.mx>
2021-05-24 17:24:45 -0500

    Finish with styles in insert cate and item

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,59 +1,74 @@
 @extends('layouts.app')
 
 @php
-    $header = 'Añadir Categoria'
+$header = 'Añadir Categoria';
 @endphp
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Añadir nueva categoria</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('categories.index') }}">Regresar</a>
+
+    <div class="flex h-screen bg-gray-200 items-center justify-center">
+        <div class="grid bg-white rounded-lg shadow-xl w-11/12 md:w-9/12 lg:w-1/2">
+            <div class="flex justify-center py-4">
+                <div class="flex bg-purple-200 rounded-full md:p-4 p-2 border-2 border-purple-300">
+                    <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
+                    </svg>
+                </div>
+            </div>
+
+            <div class="flex justify-center">
+                <div class="flex">
+                    <h1 class="text-gray-600 font-bold md:text-2xl text-xl">Añadir nueva categoria</h1>
+                </div>
+            </div>
+
+            <form action="{{ route('categories.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+
+                <div class="grid grid-cols-1 mt-5 mx-7">
+                    <label class="uppercase md:text-sm text-xs text-gray-600 text-light font-semibold">Nombre de la
+                        categoria</label>
+                    <input
+                        class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        type="text" name="name" placeholder="Ingresa el nombre" />
+                </div>
+
+                <div class="grid grid-cols-1 mt-5 mx-7">
+                    <label class="uppercase md:text-sm text-xs text-gray-600 text-light font-semibold">Descripcion de la
+                        categoria</label>
+                    <input
+                        class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        type="text" name="description" placeholder="Aqui va la descripcion" />
+                </div>
+
+                <div class="grid grid-cols-1 mt-5 mx-7">
+                    <label class="uppercase md:text-sm text-xs text-gray-600 text-light font-semibold">Imagen de la
+                        categoria</label>
+                    <input
+                        class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 border-dashed hover:bg-gray-100 hover:border-purple-300 group"
+                        type='file' class="hidden" name="image" />
+                </div>
+
+                <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+                    <a href="{{ route('categories.index') }}"
+                        class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Regresar</a>
+                    <button type="submit"
+                        class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Crear</button>
+                </div>
+            </form>
+            @if ($errors->any())
+                <div class="text-sm text-left text-red-600 bg-red-200 border border-red-400 h-auto items-center p-4 rounded-sm"
+                    role="alert">
+                    <strong>Oops! </strong>Hubo un problema
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
     </div>
-</div>
-
-@if($errors->any())
-    <div class="alert alert-danger">
-        <strong>Oops!</strong>Hubo un problema
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-<form action="{{ route('categories.store') }}" method="POST" enctype="multipart/form-data">
-
-    @csrf
-    
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Categoria:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Nombre de la categoria">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Descripcion:</strong>
-                <input type="text" name="description" class="form-control" placeholder="Descripcion de la categoria">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Imagen:</strong>
-                <input type="file" name="image" id="" class="form-control">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary">Enviar</button>
-        </div>
-    </div>
-
-</form>
 @endsection
